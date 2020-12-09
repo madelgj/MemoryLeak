040bfc22f541cc80f2b307028723f1bc7749cee0
Diego Garcia Garcia <diegodevigo@gmail.com>
2020-12-09 14:12:27 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Member.hpp"
+#include "Application.hpp"
+using namespace std;
+
+int main()
+{
+    //    Application manager();
+    //    manager.createMember("name1", "bio1", "email1", "password1");
+    //    Member userA = manager.showMembers();
+    //    cout << userA.getUsername() << endl;
+    return 0;
+}
